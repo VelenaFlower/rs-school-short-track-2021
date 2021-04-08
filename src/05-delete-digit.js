44978911bbb1d3cbf17b2498be12695f417add78
Elena Yanchenko <veleno.ajt@gmail.com>
2021-04-09 00:54:26 +0300

    feat: add my solution 05-delete-digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,21 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arr = String(n).split('');
+  let highest = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    const strArr = arr.slice();
+    strArr.splice(i, 1).map((x) => parseInt(x, 10));
+    const num = (parseInt(strArr.join(''), 10));
+
+    if (num > highest) {
+      highest = num;
+    }
+  }
+
+  return highest;
 }
 
 module.exports = deleteDigit;
