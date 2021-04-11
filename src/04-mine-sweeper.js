03494ee00a15e58be8625fd6b3c05cdfd4d8e1cb
Elena Yanchenko <veleno.ajt@gmail.com>
2021-04-11 15:20:26 +0300

    feat: add solution 04-mine-sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,34 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+
+      if (matrix[i][j - 1] || matrix[i][j + 1]) {
+        count += 1;
+      }
+      if (matrix[i + 1] !== undefined) {
+        if (matrix[i + 1][j] || matrix[i + 1][j + 1] || matrix[i + 1][j - 1]) {
+          count += 1;
+        }
+      }
+      if (matrix[i - 1] !== undefined) {
+        if (matrix[i - 1][j] || matrix[i - 1][j + 1] || matrix[i - 1][j - 1]) {
+          count += 1;
+        }
+      }
+      if (result[i]) {
+        (result[i]).push(count);
+      } else {
+        result[i] = [count];
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = minesweeper;
